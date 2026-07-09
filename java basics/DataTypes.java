c32cc4d4641a28d40b737407f0ef4ca9dd8cbd71
shruti Pandey <shrutiipandey16@gmail.com>
2026-07-09 22:59:08 +0530

    Create DataTypes.java

---
+++ java basics/DataTypes.java
@@ -0,0 +1,34 @@
+/*A data type specifies the type of data that a variable can store. It also determines the size of memory allocated and the operations, that can be performed on the data.
+Java has two types of data types:
+1. Primitive Data Types
+  - byte
+  - short
+  - int
+  - long
+  - float
+  - double
+  - char
+  - boolean
+
+  2. Non-Primitive (Reference) Data Types
+    - String
+    - Array
+    - Class
+    - Interface
+    - Object*/
+
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int number = 10;
+        float pi = 3.14f;
+        char grade = 'A';
+        boolean isJavalang = true;
+
+        System.out.println(number);
+        System.out.println(pi);
+        System.out.println(grade);
+        System.out.println(isJavalang);
+    }
+}
