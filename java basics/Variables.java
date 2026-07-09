b9a904310f610b48389b9b265f8c529e1964bd29
shruti Pandey <shrutiipandey16@gmail.com>
2026-07-09 22:54:45 +0530

    Update Variables.java

--- java basics/Variables.java
+++ java basics/Variables.java
@@ -1,1 +1,16 @@
+//A variable is a named memory location used to store data.
+//The value of a variable can be changed during program execution.
 
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 22;
+        String name = "Shruti";
+        double salary = 25000.50;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
